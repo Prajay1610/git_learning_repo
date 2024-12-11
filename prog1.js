2cceb0912ae1f031ad3b5a7758af809c588f5236
Prajay <prajaypc16@gmail.com>
2024-12-11 19:30:24 +0530

    new commit from branch1

--- prog1.js
+++ prog1.js
@@ -4,3 +4,5 @@ console.log("new rest hard checking")
 console.log("for checking 2 blob object creation")
 console.log("from branch1")
 console.log("checking two commits at a time from a main and from another branch ==> this is from branch1")
+
+console.log("change from branch1")
