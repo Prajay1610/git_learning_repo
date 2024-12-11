f7cae7772c4756aadf720f9c54a0837c3bf02df6
Prajay <prajaypc16@gmail.com>
2024-12-11 19:01:31 +0530

    simultaneous commit from main

--- prog1.js
+++ prog1.js
@@ -3,3 +3,5 @@ console.log("added for second commit)
 console.log("new rest hard checking")
 console.log("for checking 2 blob object creation")
 console.log("from branch1")
+
+console.log("simultaneous commit from main on different line")
